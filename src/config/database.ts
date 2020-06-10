01a0ab90e297b1f80ce30b32343053c410f8076a
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 15:48:44 +0200

    Change migration directory in config due to error
    This has something to do with import statement in the migration file
    @issue

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,3 +1,5 @@
+import { join } from 'path';
+
 const ORMConfig = {
     driver: 'mysq',
     type: 'mysql',
@@ -6,12 +8,12 @@ const ORMConfig = {
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
     port: parseInt(process.env.TYPEORM_PORT, 10),
-    entities: [__dirname + '/../**/**.entity{.ts,.js}'],
+    entities: [join(__dirname, '/../**/**.entity{.ts,.js}')],
     synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
     migrationsTableName: "custom_migration_table",
-    migrations: ["src/migration/*.ts"],
+    migrations: ["migration/*.ts"],
     cli: {
-        migrationsDir: "src/migration"
+        migrationsDir: "migration"
     },
 };
 
