0a4f8df6ed43e6c78145f1774ff82a0cd1c34e2e
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 15:49:27 +0200

    Deserialize user

--- src/serializers/session.serializer.ts
+++ src/serializers/session.serializer.ts
@@ -1,14 +1,23 @@
 import { PassportSerializer } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { UserDto } from 'src/dto/UserDto';
+import { UserService } from 'src/services/user.service';
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  serializeUser(user: UserDto, done: (err: Error, user: UserDto) => void): any {
-    console.log('serializer user', user);
-    
-    done(null, user);
+  constructor(private readonly userService: UserService) {
+    super();
   }
-  deserializeUser(payload: any, done: (err: Error, payload: string) => void): any {
-    done(null, payload);
+  serializeUser(user: UserDto, done: (err: Error, user: string) => void): any {
+    done(null, user.username);
+  }
+  async deserializeUser(username: string, done: (err: Error, user: string) => void): Promise<any> {
+    try {
+      const result = await this.userService.findByName(username);
+      done(null, result.username);
+    }
+    catch (error) {
+      done(error, username);
+    }
+    
   }
 }
