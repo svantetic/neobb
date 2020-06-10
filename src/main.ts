c33393d432a9f6ffc901004f6d2fdba2874232a4
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 15:20:48 +0200

    Use session and local strategy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as nunjucks from 'nunjucks';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import flash = require('connect-flash');
 
 const port = 8080;
 
@@ -20,6 +23,19 @@ async function bootstrap() {
       express: app
     }
   );
+
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+
   app.engine('njk', renderEngine.render);
   app.useStaticAssets(join(__dirname, '../public'), {
     index: false,
