0e09c75739b11fdf7e4a3427a97d75c2cfc47de1
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 15:21:32 +0200

    Replace name with username to be consistent

--- src/dto/UserDto.ts
+++ src/dto/UserDto.ts
@@ -2,32 +2,32 @@ import * as joi from 'joi';
 
 export const userSchema = joi.object({
     password: joi.string().required(),
-    name: joi.string().required(),
+    username: joi.string().required(),
 });
 
 export const registerUserSchema = joi.object({
     password: joi.string().required(),
-    name: joi.string().required(),
+    username: joi.string().required(),
     email: joi.string().required(),
 });
 
 export const adminUserSchema = joi.object({
     password: joi.string().required(),
-    name: joi.string().required(),
+    username: joi.string().required(),
 })
 
 export class UserDto {
-    readonly name: string;
+    readonly username: string;
     readonly password: string;
 }
 
 export class RegisterUserDto {
-    readonly name: string;
+    readonly username: string;
     readonly password: string;
     readonly email: string;
 }
 
 export class AdminUserDto {
-    readonly name: string;
+    readonly username: string;
     readonly password: string;
 }
