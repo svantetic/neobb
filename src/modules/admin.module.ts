fb203f528c3bad3ed66e43489b46709100237033
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 15:23:51 +0200

    Delete passport initialization from admin module

--- src/modules/admin.module.ts
+++ src/modules/admin.module.ts
@@ -1,22 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from 'src/services/user.service';
-import { UserController } from 'src/controllers/user.controller';
 import { User } from 'src/model/user.entity';
 import { AdminController } from 'src/controllers/admin.controller';
 import { AuthService } from 'src/services/auth.service';
-import { JwtStrategy } from 'src/strategy/jwt.strategy';
-import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-        // TODO: remove temp key and use .env
-        secret: 'temporary_key',
-    }),
   ],
   controllers: [AdminController],
-  providers: [UserService, AuthService, JwtStrategy],
+  providers: [UserService, AuthService],
 })
 export class AdminModule {}
