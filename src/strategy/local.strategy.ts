8e0d3086690ea4d93ac191b1b87bd2add4bfa871
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 15:49:34 +0200

    Remove console logs

--- src/strategy/local.strategy.ts
+++ src/strategy/local.strategy.ts
@@ -10,10 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(username: string, password: string): Promise<any> {
-        console.log('validate');
-        
         const _user = await this.authService.validateUser({ username, password });
-        console.log('user in local strategy', _user);
         
         if (!_user) {
             throw new UnauthorizedException();
