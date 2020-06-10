7101128928a645f553aafcd0d7c505e9fee213e2
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-10 21:54:07 +0200

    Import required modules in section module

--- src/modules/section.module.ts
+++ src/modules/section.module.ts
@@ -4,12 +4,16 @@ import { Section } from '../model/section.entity';
 import { SectionController } from '../controllers/section.controller';
 import { SectionService } from '../services/section.service';
 import { Segment } from '../model/segment.entity';
+import { ThreadService } from 'src/services/thread.service';
+import { UserService } from 'src/services/user.service';
+import { Thread } from 'src/model/thread.entity';
+import { User } from 'src/model/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Section, Segment]),
+    TypeOrmModule.forFeature([Section, Segment, Thread, User]),
   ],
   controllers: [SectionController],
-  providers: [SectionService],
+  providers: [SectionService, ThreadService, UserService],
 })
 export class SectionModule {}
