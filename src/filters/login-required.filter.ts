4096cb8e7a464059e72d9b15cc1e0334a59ede5d
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-11 13:22:16 +0200

    Create login required filter

---
+++ src/filters/login-required.filter.ts
@@ -0,0 +1,19 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, UnauthorizedException, ForbiddenException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class LoginRequiredFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    if (
+        exception instanceof UnauthorizedException ||
+        exception instanceof ForbiddenException
+     ) {
+       request.flash('loginRequired', 'Login is required');
+       response.redirect('login');
+    }
+  }
+}
