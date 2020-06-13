58b8dc33b04e862fd36125b3a0327263efc120a4
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-13 19:15:30 +0200

    Update improts in user module

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -4,16 +4,10 @@ import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
 import { User } from '../model/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from '../strategy/jwt.strategy';
-import { ConfigService, ConfigModule } from 'nestjs-config';
-import * as path from 'path';
 import { LocalStrategy } from 'src/strategy/local.strategy';
 @Module({
   imports: [
-    ConfigModule,
-    ConfigModule.load(path.resolve(__dirname, 'config', '**', '!(*.d).{ts,js}')),
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'local' }),
   ],
