217a95ca58a2b15fceb147ec7838122470fab451
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-13 19:17:49 +0200

    Comment out unused jwt strategy

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -2,23 +2,23 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from '../services/auth.service';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService, InjectConfig } from 'nestjs-config';
+import { ConfigService } from '@nestjs/config';
 
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService, @InjectConfig() private readonly config: ConfigService) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // TODO: remove temp key and use .env
-      secretOrKey: 'temporary_key',
-    });
-  }
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy) {
+//   constructor(private readonly authService: AuthService, @InjectConfig() private readonly config: ConfigService) {
+//     super({
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       // TODO: remove temp key and use .env
+//       secretOrKey: 'temporary_key',
+//     });
+//   }
 
-  async validate(payload: string) {
-    // // const user = await this.authService.validateUser(payload);
-    // if (!user) {
-    //   throw new UnauthorizedException();
-    // }
-    // return user;
-  }
-}
+//   async validate(payload: string) {
+//     // // const user = await this.authService.validateUser(payload);
+//     // if (!user) {
+//     //   throw new UnauthorizedException();
+//     // }
+//     // return user;
+//   }
+// }
