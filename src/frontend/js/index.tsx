8eb61d9c6de832fb598d3b12bfaa14d92b2b77f7
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-13 21:51:48 +0200

    Create React build pipeline

---
+++ src/frontend/js/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <p>This is app</p>;
+
+ReactDOM.render(<App />, document.getElementById('post-mount'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
-/*
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+    entry: './src/frontend/js/index.tsx',
     mode: 'development',
-    entry: './src/frontend/css/app.css',
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                          ident: 'postcss',
-                          plugins: [
-                            require('tailwindcss'),
-                            require('autoprefixer'),
-                          ],
-                        },
-                      },
-                  ],
+                test: /\.(ts|tsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
             }
         ]
     },
+    resolve: {
+        extensions: ['*', '.ts', '.tsx', '.js', '.jsx'],
+    },
     output: {
-        filename: 'app.css',
-        path: path.resolve(__dirname, 'public/css'),
-    }
+        path: path.resolve(__dirname, 'public/js'),
+        publicPath: '/public/js',
+        filename: 'bundle.js',
+    },
 }
-*/
