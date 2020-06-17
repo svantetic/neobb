4bccd610d0547b618a50018468a6d91d453f72ba
Arkadiusz Mierzwa <arkadiusm@gmail.com>
2020-06-18 00:57:55 +0200

    Add creating posts logic

--- src/frontend/js/components/PostForm.vue
+++ src/frontend/js/components/PostForm.vue
@@ -2,7 +2,7 @@
     <form @submit="handleSubmit">
         <label for="content" class="block">Content</label>
         <textarea v-model="content" required class="shadow-xl block w-full mb-10 p-4" id="content" cols={30} rows={10} name="content"></textarea>
-        <Button @button-click="createPost">Reply</Button>
+        <Button type="submit">Reply</Button>
     </form>
 </template>
 
@@ -10,14 +10,35 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import Button from '../common/Button.vue';
+
+declare global {
+    interface Window {
+        threadId: string | number;
+    }
+}
+
 @Component({
     components: { Button }
 })
 export default class PostForm extends Vue {
     content: string = '';
 
-    handleSubmit(event: Event) {
-        event.preventDefault();
+    async handleSubmit(event: Event) {
+                event.preventDefault();
+        try {
+            const response = await fetch('/post', {
+                method: 'POST',
+                headers: {
+                    "Content-Type" : "application/json",
+                },
+                body: JSON.stringify({ content: this.content, threadId: window.threadId, }),
+            });
+            if (response.ok) {
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     createPost() {
